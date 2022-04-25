0b6bb3c1231a414d9dafc6218b54f767dbc86df5
Gago <egagik69@gmail.com>
2022-04-25 13:31:48 +0400

    spread

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
-// function lotto() {
-
-//     let arr = [];
-//     for (let i = 0; i < 90; i++) {
-//         arr.push(i);
-//     }
-
-//     return function randomNum() { 
-//        let ran = Math.floor(Math.random() * (arr.length - 1));
-//        let i =  arr.splice(ran, 1);
-//        return i[0]
-//     }
-// }
-
-// let lottoStart = lotto();
-
-
-// for(let i = 0; i< 90; i++){
-//     console.log(lottoStart());
-// }
-
-
-
-function* lotto() {
-    let arr = [];
-    for (let i = 1; i < 90; i++) {
-        arr.push(i);
-    }
-
-    for (let j = 1; j < arr.length; j++) {
-        let ran = Math.floor(Math.random() * (arr.length - 1));
-        let i = arr.splice(ran, 1);
-        yield i[0]
-    }
-}
-
-for (let value of lotto()) {
-    console.log(value)
-}
+const btn = document.getElementById('btn');
+let arr = [2, 3, 4, 5, 6, 7, 8, 9];
+let i = 777;
+btn.addEventListener('click', function showArr() {
+    let [, ...arg] = arr;
+    arr = arg;
+    let addArr = [...arr, i];
+    i += 1;
+    arr = addArr;
+    console.log(arr);
+})
