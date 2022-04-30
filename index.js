c096995e81e6f30b88c64f3ff557c4dcb3329c6a
Gago <egagik69@gmail.com>
2022-04-30 23:54:49 +0400

    check brackets

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const btn = document.getElementById('btn');
-let arr = [2, 3, 4, 5, 6, 7, 8, 9];
-let i = 777;
-btn.addEventListener('click', function showArr() {
-    let [, ...arg] = arr;
-    arr = arg;
-    let addArr = [...arr, i];
-    i += 1;
-    arr = addArr;
-    console.log(arr);
-})
+const url = 'https://my-json-server.typicode.com/AshotAleqs/exams/expression';
+let obj = {}
+fetch(url)
+    .then((res) => {
+        return res.json()
+    })
+    .then((example) => {
+        console.log(example)
+    })
+
+let date1 = '(){}}[]';
+let date2 = '((){}[])';
+
+function checkDate(date) {
+    let brackets = "[]{}()"
+    let stack = []
+    for (let i = 0; i < date.length; i++) {
+        let bracketIndex = brackets.indexOf(date[i]);
+        if (bracketIndex % 2 === 0) {
+            stack.push(bracketIndex + 1);
+        } else {
+            if(stack.pop()!== bracketIndex){
+                return false
+            }
+        }
+    }
+    return stack.length === 0
+}
+
+checkDate(date2)
