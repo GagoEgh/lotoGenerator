56b2000a2d283cbeeb08e12dc48bf1a5b00cc21a
Gago <egagik69@gmail.com>
2022-05-06 10:11:44 +0400

    expression, calculation, flatten

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ fetch(url)
         console.log(example)
     })
 
+//1.  expression
 let date1 = '(){}}[]';
 let date2 = '((){}[])';
 
@@ -19,12 +20,77 @@ function checkDate(date) {
         if (bracketIndex % 2 === 0) {
             stack.push(bracketIndex + 1);
         } else {
-            if(stack.pop()!== bracketIndex){
+            if (stack.pop() !== bracketIndex) {
                 return false
             }
         }
     }
     return stack.length === 0
 }
+console.log(checkDate('({})'));
 
-checkDate(date2)
+
+//2 random calculation
+numbers = [2, 3, 4, 5, -6, 9, 3,];
+
+function myMath(numbers) {
+    let mathOperators = ['+', '-', '/', '*'];
+    let res = 0;
+    let count = mathOperators.length;
+
+    for (let i = 0; i < numbers.length - 1; i++) {
+
+        if (numbers[i] < 0) {
+            let index = (i % count) - 2;
+            if (i % count === 0) {
+                index = 2
+            }
+
+            let tmp;
+            tmp = mathOperators[index];
+            mathOperators[index] = mathOperators[i % count];
+            mathOperators[i % count] = tmp;
+
+        }
+
+        if (mathOperators[i % count] === '+') {
+            res = numbers[i] + numbers[i + 1];
+
+        } else if (mathOperators[i % count] === '-') {
+            res = res - numbers[i + 1];
+
+        } else if (mathOperators[i % count] === '*') {
+            res = res * numbers[i + 1];
+
+        } else if (mathOperators[i % count] === '/') {
+            res = res / numbers[i + 1];
+        }
+
+        console.log(res);
+
+    }
+}
+
+console.log(numbers);
+myMath(numbers)
+
+//3 flatten
+function myFlatten() {
+    let result = [];
+    return function pushDate(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                pushDate(arr[i]);
+            } else {
+                result.push(arr[i]);
+
+            }
+        }
+        return result
+    }
+}
+
+let arr = [3, 4, [33, 44, [9, 8]], [45, 43]];
+let flat = myFlatten();
+console.log(flat(arr))
+
